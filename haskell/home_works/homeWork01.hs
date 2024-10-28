44bf612bb5bcf4f47a8aa6227bb57869e0ab9604
ciceropaulino <cicero.filho.069@ufrn.edu.br>
2024-10-28 13:07:59 -0300

    Declaring some syntatic sugar and defining multiplication

--- haskell/home_works/homeWork01.hs
+++ haskell/home_works/homeWork01.hs
@@ -5,7 +5,24 @@ import Prelude hiding (Num (..))
 -- Naturals definition
 data Nat = O | S Nat deriving (Eq, Show)
 
--- Question 1. Define (without consultation) addition, multiplication, and exponentiation (in Nats).
+-- [Question 1.] Define (without consultation) addition, multiplication, and exponentiation (in Nats).
+
+-- Defining sum
 (+) :: Nat -> Nat -> Nat
 n + O = n
 n + (S m) = S (n + m)
+
+-- Defining multiplication
+(*) :: Nat -> Nat -> Nat
+n * O = O
+n * (S m) = (n * m) + n
+
+-- [Syntactic Sugar]
+o, so, sso, ssso, sssso, ssssso, sssssso :: Nat
+o = O
+so = S o
+sso = S so
+ssso = S sso
+sssso = S ssso
+ssssso = S sssso
+sssssso = S ssssso
