38707c968d9630fe3c872b46bf13561193869060
ciceropaulino <cicero.filho.069@ufrn.edu.br>
2024-10-27 23:42:03 -0300

    Defining Sum

--- haskell/home_works/homeWork01.hs
+++ haskell/home_works/homeWork01.hs
@@ -1,4 +1,11 @@
 module Nat where
 
+import Prelude hiding (Num (..))
+
 -- Naturals definition
 data Nat = O | S Nat deriving (Eq, Show)
+
+-- Question 1. Define (without consultation) addition, multiplication, and exponentiation (in Nats).
+(+) :: Nat -> Nat -> Nat
+n + O = n
+n + (S m) = S (n + m)
